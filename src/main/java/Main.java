173788fab731f8579e80548ee244580a745f6e56
sergeyonischuk <sergey.onischuk@ukr.net>
2023-12-14 21:43:52 +0200

    started creating the visual part with java-fx instruments.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,11 @@
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 import org.apache.commons.io.FileUtils;
 
 import java.io.File;
@@ -6,10 +14,40 @@ import java.time.Month;
 import java.util.HashSet;
 import java.util.List;
 
-public class Main {
+public class Main extends Application {
 
-    public static void main(String[] args) throws IOException {
-        FileUtils.cleanDirectory(new File("src/main/result"));
+    public static void main(String[] args) {
+        launch(args);
+
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Моя програма");
+
+        ChoiceBox<Month> monthChoiceBox = new ChoiceBox<>();
+        monthChoiceBox.getItems().addAll(Month.values());
+        monthChoiceBox.setValue(Month.JANUARY);
+
+        Button createFilesButton = new Button("Створити файли");
+        createFilesButton.setOnAction(e -> createFilesForCurrentMonth(monthChoiceBox.getValue()));
+
+        VBox vbox = new VBox(10);
+        vbox.setPadding(new Insets(10));
+        vbox.getChildren().addAll(monthChoiceBox, createFilesButton);
+
+
+        Scene scene = new Scene(vbox, 300, 200);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    private void createFilesForCurrentMonth(Month value) {
+        try {
+            FileUtils.cleanDirectory(new File("src/main/result"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         ExcelReader reader = new ExcelReader();
 
         ClientService service = new ClientService();
@@ -18,8 +56,7 @@ public class Main {
         HashSet<String> ativePhones = reader.getUniquePhoneNumbersFromSells("src/main/resources/sales.xls");
         List<Client> activeClientList = service.getActiveClients(clientList, ativePhones);
 
-
-
-        phoneNumberWriter.writePhoneNumbersByDay(service.getClientsByMonth(activeClientList, Month.DECEMBER), Month.DECEMBER, 2023);
+        phoneNumberWriter.writePhoneNumbersByDay(service.getClientsByMonth(activeClientList, value), value, 2023);
+        System.out.println("Ти подиви, працює!");
     }
 }
